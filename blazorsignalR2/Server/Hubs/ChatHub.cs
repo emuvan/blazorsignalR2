3f54b5cc18bf6f2080f43bfe0152b0be8e3faf13
van <van.nguyen35@gmail.com>
2021-10-05 11:17:29 +0100

    Add project files.

---
+++ blazorsignalR2/Server/Hubs/ChatHub.cs
@@ -0,0 +1,13 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.SignalR;
+
+namespace blazorsignalR2.Server.Hubs
+{
+    public class ChatHub : Hub
+    {
+        public async Task SendMessage(string name, string message)
+        {
+            await Clients.All.SendAsync("SendMessage", name, message);
+        }
+    }
+}
